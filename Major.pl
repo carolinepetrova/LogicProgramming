f33fb3264272c41e6257f981822a64bc95e432e0
Karolina Petrova <karolinapetrovawork@gmail.com>
2020-01-10 00:45:58 +0200

    Update Major.pl

--- Major.pl
+++ Major.pl
@@ -6,24 +6,14 @@ append([H|T],L,[H|L1]):-append(T,L,L1).
 
 insert(H,L,LR):-append(A,B,L),append(A,[H|B],LR).
 
-simpleListSort(L,LR):-permutate(L,LR),isSorted(LR).
-
-length2([],0).
-length2([_|T],N):-length2(T,M),N is M+1.
-
-isSorted([H1,H2]):-length2(H1,L1),length(H2,L2),L1=<L2.
-isSorted([H,H1|T]):-length2(H,L1),length(H1,L2),L1=<L2,isSorted([H1|T]).
-
 member(H,[H|_]).
 member(X,[_|T]):-member(X,T).
 
 isSubset([],_).
 isSubset([H|T],L):-member(H,L),isSubset(T,L).
 
-not_major(_,[]).
-not_major(X,[H|T]):-not(isSubset(X,H)),not_major(X,T).
+major_checker([_]).
+major_checker([H|T]):-not((member(X,T),isSubset(X,H))),major_checker(T),!.
 
 %p(L,M)
-p([],[]).
-p([H|T],R):-not(not_major(H,T)),p(T,R).
-p([H|T],[H|R]):-not_major(H,T),p(T,R).
+p(L,R):-permutate(L,R),major_checker(R).
